1034c5c15df9aa59a746ebf43b06554df4e54909
namanagrawal22 <naman.a@consultadd.com>
2025-07-29 10:18:39 -0400

    changes to ingest elastic_docs

--- semantic-indexer.py
+++ semantic-indexer.py
@@ -1,18 +1,26 @@
-from langchain_community.document_loaders import PyPDFLoader
+from langchain.document_loaders import DirectoryLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from sources.elasticsearch import load_elasticsearch
 from elasticsearch import helpers
 
 
-loader = PyPDFLoader("https://arxiv.org/pdf/2103.15348.pdf")
-
 client = load_elasticsearch()
+
+
+loader = DirectoryLoader(
+    path="./elastic_docs",
+    glob="**/*.txt",
+    loader_cls=TextLoader,
+    show_progress=True
+)
+
 
 data = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
     chunk_size=512, chunk_overlap=256
 )
+
 docs = loader.load_and_split(text_splitter=text_splitter)
 
 helpers.bulk(
